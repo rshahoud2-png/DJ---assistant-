679ef619897aabfa32c7ffe7de54fe7c9c2b6cfd
rshahoud2-png <rshahoud2@gmail.com>
2026-06-19 15:20:55 -0400

    Export recommendation engine

---
+++ src/lib/recommendation/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./setlist-builder";
+export * from "./scoring";
+export * from "./compatibility";
+export * from "./energy-curve";
+export * from "./key-matching";
+export * from "./bpm-transition";
